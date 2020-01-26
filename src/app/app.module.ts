25a324a9777a628507bbc2730a7185ba6fbae2b2
nigmasilmi <nigmasilmi.web@gmail.com>
2020-01-26 20:28:13 -0300

    Created Client Component and Server Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientComponent } from './components/client/client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule,
