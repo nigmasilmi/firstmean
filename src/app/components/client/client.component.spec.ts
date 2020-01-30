6763ff7adf41bd6c9513245ce43d27894ba85685
nigmasilmi <nigmasilmi.web@gmail.com>
2020-01-30 17:48:09 -0300

    fixed integration tests

--- src/app/components/client/client.component.spec.ts
+++ src/app/components/client/client.component.spec.ts
@@ -1,14 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ServerService } from '../../services/server.service';
 
 import { ClientComponent } from './client.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import {
-  HttpClientTestingModule,
-  HttpTestingController
-} from "@angular/common/http/testing";
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+  HttpClientTestingModule
+} from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 
 describe('ClientComponent', () => {
   let component: ClientComponent;
@@ -18,11 +18,11 @@ describe('ClientComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ClientComponent],
-      imports:[
-        MatButtonModule, 
-        MatListModule, 
-        MatIconModule, 
-        HttpClientTestingModule, 
+      imports: [
+        MatButtonModule,
+        MatListModule,
+        MatIconModule,
+        HttpClientTestingModule,
 
       ]
     }).compileComponents();
--- src/app/services/server.service.spec.ts
+++ src/app/services/server.service.spec.ts
@@ -1,27 +1,24 @@
-import { TestBed } from "@angular/core/testing";
-import {
-  HttpClientTestingModule,
-  HttpTestingController
-} from "@angular/common/http/testing";
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { TestBed } from '@angular/core/testing';
 
-import { ServerService } from "./server.service";
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
-describe("ServerService", () => {
+import { ServerService } from './server.service';
+
+describe('ServerService', () => {
   let httpClient: HttpClient;
-  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
+      imports: [HttpClientModule],
+      providers: [HttpClient]
     });
 
     // Inject the http service and test controller for each test
     httpClient = TestBed.get(HttpClient);
-    httpTestingController = TestBed.get(HttpTestingController);
   });
 
-  it("should be created", () => {
+  it('should be created', () => {
     const service: ServerService = TestBed.get(ServerService);
     expect(service).toBeTruthy();
   });
