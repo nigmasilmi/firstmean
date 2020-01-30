df671ed864ff3d249a9087ac3c4fbebcc7b5a8d0
nigmasilmi <nigmasilmi.web@gmail.com>
2020-01-30 18:37:38 -0300

    app.po and app.e2e pointing at the same app selector

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('reignFSTest');
+    expect(page.getTitleText()).toEqual('HN Feed');
   });
 
   afterEach(async () => {
